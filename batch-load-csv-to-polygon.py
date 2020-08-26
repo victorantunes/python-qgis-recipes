c0fd199ed1c7d6886d988fbd611afea35a58fda1
Victor Antunes <victorantunes@users.noreply.github.com>
2020-08-25 22:20:34 -0300

    Update batch-load-csv-to-polygon.py
    Added comments
    Restructured code

--- batch-load-csv-to-polygon.py
+++ batch-load-csv-to-polygon.py
@@ -1,13 +1,9 @@
 # Copy and paste this script directly into QGIS Python Console (Ctrl+Alt+P).
-# This script loads latitude/longitude pairs .csv files as polygons.
-# Lines in the file are treated as individual points to serve as bounds for the polygon.
-# The fill colors of the polygons are random.
-# Algorithms used are "qgis:pointstopath" and "qgis:linestopolygons".
+# This script loads latitude/longitude .csv files as red line layers.
+# Lines in the file are treated as individual points.
+# The algorithm "qgis:pointstopath" converts your points to a single path.
 # EPSG:4326 is WGS-84 and should be changed to suit your dataset (https://epsg.io/)
-# The layer is loaded into memory only. User must export layers manually.
-# A label is added to the polygon. User must enable the label manually.
-# The label consists of the file's name, with the .csv extension removed.
-# Input files must have a header describing the columns.
+# Files must have a header describing the columns.
 #
 # Example input file:
 #
@@ -16,13 +12,14 @@
 # -15.51251251213123,-37.857192873412312
 # -15.51254151231232,-37.123897123987123
 
+
 import os, glob
 from qgis import processing
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtCore import QVariant
 from PyQt5.QtGui import QColor
 
-basePath = '<basepath>'
+basePath = '<basePath>'
 baseUri = "file://"+basePath
 
 instance = QgsProject.instance()
@@ -30,15 +27,28 @@ instance = QgsProject.instance()
 ext = ".csv"
 
 for file in glob.glob(os.path.join(basePath, "*%s" % ext)):
+    # remove .csv extension
     line = ".".join(file.split("/")[-1].split(".")[:-1])
-    uri = os.path.join(baseUri, "%s%s?encoding=%s&delimiter=%s&xField=%s&yField=%s&crs=%s" % (line, ext, "UTF-8",",", "longitude", "latitude","epsg:4326"))
+    # encoding, delimiter, xField, yField, crs
+    uri = os.path.join(
+      baseUri,
+       "%s%s?encoding=%s&delimiter=%s&xField=%s&yField=%s&crs=%s" 
+       % (line, ext, "UTF-8",",", "longitude", "latitude","epsg:4326"))
     points = QgsVectorLayer(uri, line, "delimitedtext")
+    # order by feature id
     points.addExpressionField('abs($id)', QgsField('index', QVariant.Int))
-    paths = processing.run('qgis:pointstopath', {'INPUT':points, 'ORDER_FIELD':'index', 'OUTPUT':'TEMPORARY_OUTPUT'})['OUTPUT']
-    polygon = processing.run('qgis:linestopolygons',{'INPUT': paths, 'OUTPUT': 'TEMPORARY_OUTPUT'})['OUTPUT']
+    # vertices into single path, loaded onto memory
+    paths = processing.run('qgis:pointstopath',
+      {'INPUT': points,
+        'ORDER_FIELD':'index',
+        'OUTPUT':'TEMPORARY_OUTPUT'})['OUTPUT']
+    # path into polygon, loaded onto memory
+    polygon = processing.run('qgis:linestopolygons', 
+      {'INPUT': paths,
+       'OUTPUT': 'TEMPORARY_OUTPUT'})['OUTPUT']
     polygon.setName(line)
+    # label styling
     polygon.setLabelsEnabled(True)
-    layer_settings  = QgsPalLayerSettings()
     text_format = QgsTextFormat()
     text_format.setFont(QFont("Arial", 8))
     text_format.setSize(8)
@@ -47,10 +57,12 @@ for file in glob.glob(os.path.join(basePath, "*%s" % ext)):
     buffer_settings.setSize(0.10)
     buffer_settings.setColor(QColor("black"))
     text_format.setBuffer(buffer_settings)
+    layer_settings  = QgsPalLayerSettings()
     layer_settings.setFormat(text_format)
     layer_settings.fieldName = "@layer_name"
     layer_settings.placement = 4
     layer_settings.enabled = True
     layer_settings = QgsVectorLayerSimpleLabeling(layer_settings)
     polygon.setLabeling(layer_settings)
+    # add to map
     instance.addMapLayer(polygon)
